c5c17b13194046e13d906cf38f0f3471943824b8
Arafat islam <arafat.innotimes.net@gmail.com>
2024-05-10 02:12:18 +0600

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://cardoctor-bd.web.app",
-      "https://cardoctor-bd.firebaseapp.com",
     ],
     credentials: true,
   })
@@ -16,10 +16,16 @@ app.use(
 
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2lraink.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ykgi9mv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -27,18 +33,48 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
 async function run() {
   try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // user collection
+    const userCollection = client.db('assainment11').collection("users");
 
-
+    app.get('/users', async (req, res) => {
+      const cursor = userCollection.find()
+      const result = await cursor.toArray();
+      res.send(result)
+    });
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log('new user ', user)
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+    });
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
+
+// async function run() {
+//   try {
+
+
+
+
+
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
