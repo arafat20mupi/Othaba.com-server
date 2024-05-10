fff9bedb81ea3c1b8e5cbf049d6b9cb9dc82fc60
Arafat islam <arafat.innotimes.net@gmail.com>
2024-05-10 18:08:07 +0600

    thard commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
@@ -17,14 +17,6 @@ app.use(
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ykgi9mv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -35,9 +27,6 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // user collection
     const userCollection = client.db('assainment11').collection("users");
 
     app.get('/users', async (req, res) => {
@@ -51,12 +40,23 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result)
     });
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
+
+    app.get('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const quary = { _id: ObjectId.createFromHexString(id) }
+      const user = await userCollection.findOne(quary)
+      res.send(user)
+    });
+
+    app.get('/user/last', async (req, res) => {
+      const cursor = userCollection.find().sort({_id: -1}).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+
   }
 }
 run().catch(console.dir);
