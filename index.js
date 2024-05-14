c69754ecdd1c70a98b9ca7a26ead99dde03f94cd
Arafat islam <arafat.innotimes.net@gmail.com>
2024-05-14 13:35:13 +0600

    recomendation Count Add & deleta

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-    ],
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+  ],
+  credentials: true,
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ykgi9mv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -33,13 +31,11 @@ async function run() {
     const RecommendedCollection = client.db('assainment11').collection("Recommended");
 
     // Routes for user collection
-
     app.get('/users', async (req, res) => {
       const cursor = QuariesCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
-
 
     app.post('/users', async (req, res) => {
       const user = req.body;
@@ -49,51 +45,29 @@ async function run() {
 
     app.get('/users/:id', async (req, res) => {
       const id = req.params.id;
-      const quary = { _id: ObjectId.createFromHexString(id) };
-      const user = await QuariesCollection.findOne(quary);
+      const query = { _id: ObjectId.createFromHexString(id) };
+      const user = await QuariesCollection.findOne(query);
       res.send(user);
     });
-    // app.get('/users/:id', async (req, res) => {
-    //   try {
-    //     const id = req.params.id;
-    //     const query = { _id: ObjectId.createFromHexString(id) };
-
-    //     // Find the user by ID and increment recommendation_count
-    //     const user = await QuariesCollection.findOneAndUpdate(
-    //       query,
-    //       { $inc: { recommendation_count: 1 } },
-    //       { returnOriginal: false } // To return the updated document
-    //     );
-
-    //     if (!user.value) {
-    //       // If user not found, return 404
-    //       return res.status(404).send("User not found");
-    //     }
-
-    //     // Return the updated user data
-    //     res.send(user.value);
-    //   } catch (error) {
-    //     console.error("Error updating user:", error);
-    //     res.status(500).send("Internal Server Error");
-    //   }
-    // });
-
 
     app.get('/user/last', async (req, res) => {
       const cursor = QuariesCollection.find().sort({ _id: -1 }).limit(6);
       const result = await cursor.toArray();
       res.send(result);
     });
+
     app.get('/new', async (req, res) => {
       const cursor = QuariesCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
+
     app.get('/new/:email', async (req, res) => {
       const email = req.params.email;
       const data = await QuariesCollection.find({ email: email }).toArray();
       res.send(data);
     });
+
     app.post('/new', async (req, res) => {
       const user = req.body;
       const result = await QuariesCollection.insertOne(user);
@@ -102,15 +76,15 @@ async function run() {
 
     app.delete("/new/:id", async (req, res) => {
       const id = req.params.id;
-      const quary = { _id: ObjectId.createFromHexString(id) };
-      const result = await QuariesCollection.deleteOne(quary);
+      const query = { _id: ObjectId.createFromHexString(id) };
+      const result = await QuariesCollection.deleteOne(query);
       res.send(result);
     });
 
     app.get('/new/:id', async (req, res) => {
       const id = req.params.id;
-      const quary = { _id: ObjectId.createFromHexString(id) };
-      const user = await QuariesCollection.findOne(quary);
+      const query = { _id: ObjectId.createFromHexString(id) };
+      const user = await QuariesCollection.findOne(query);
       res.send(user);
     });
 
@@ -130,23 +104,89 @@ async function run() {
       res.send(result);
     });
 
+    // New route to increment recommendation count
+    app.patch('/updateRecommendationCount/:id', async (req, res) => {
+      try {
+        const id = req.params.id;
+        const filter = { _id: ObjectId.createFromHexString(id) };
+        const updateDoc = {
+          $inc: { recommendation_count: 1 }
+        };
+
+        const result = await QuariesCollection.updateOne(filter, updateDoc);
+
+        if (result.matchedCount === 0) {
+          return res.status(404).send("Document not found");
+        }
+
+        res.send(result);
+      } catch (error) {
+        console.error("Error updating recommendation count:", error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+
     // Routes for recommended collection
     app.get('/recommended', async (req, res) => {
       const cursor = RecommendedCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
-
+    // ---------------------
     app.post('/recommended', async (req, res) => {
-      const user = req.body;
-      const result = await RecommendedCollection.insertOne(user);
-      res.send(result);
+      const recommendation = req.body;
+
+      try {
+        // Add recommendation to RecommendedCollection
+        const result = await RecommendedCollection.insertOne(recommendation);
+
+        // Increment recommendation_count in QuariesCollection
+        const queryId = recommendation.quaryId;
+        await QuariesCollection.updateOne(
+          { _id: ObjectId.createFromHexString(queryId) },
+          { $inc: { recommendation_count: 1 } }
+        );
+
+        res.send(result);
+      } catch (error) {
+        console.error('Error adding recommendation:', error);
+        res.status(500).send("Internal Server Error");
+      }
     });
 
+
+
+    
+    app.delete("/recommended/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: ObjectId.createFromHexString(id) };
+
+        // Fetch the recommendation to get the associated query ID
+        const recommendation = await RecommendedCollection.findOne(query);
+        const queryId = recommendation.quaryId;
+
+        // Delete the recommendation
+        const result = await RecommendedCollection.deleteOne(query);
+
+        // Decrement recommendation_count in QuariesCollection
+        await QuariesCollection.updateOne(
+          { _id: ObjectId.createFromHexString(queryId) },
+          { $inc: { recommendation_count: -1 } }
+        );
+
+        res.send(result);
+      } catch (error) {
+        console.error('Error deleting recommendation:', error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+
+    // -----------------
     app.delete("/recommended/:id", async (req, res) => {
       const id = req.params.id;
-      const quary = { _id: ObjectId.createFromHexString(id) };
-      const result = await RecommendedCollection.deleteOne(quary);
+      const query = { _id: ObjectId.createFromHexString(id) };
+      const result = await RecommendedCollection.deleteOne(query);
       res.send(result);
     });
 
@@ -167,6 +207,7 @@ async function run() {
       const data = await RecommendedCollection.find({ Recommender_Email: email }).toArray();
       res.send(data);
     });
+
     app.get('/recommendationMe/:email', async (req, res) => {
       const email = req.params.email;
       const data = await RecommendedCollection.find({ userEmail: email }).toArray();
